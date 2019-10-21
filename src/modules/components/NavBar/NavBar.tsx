f8ae1927887530c2f795761dc38c8d693f4b3632
Chuks Grinage <chuksgrinage@gmail.com>
2019-10-20 22:06:27 -0500

    establish layout and archetecture

---
+++ src/modules/components/NavBar/NavBar.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import styles from './NavBar.module.scss';
+
+export default class NavBar extends React.Component {
+
+  render() {
+    return (
+      <div className={styles.navBar}>
+        navbar
+      </div>
+    );
+  }
+}
