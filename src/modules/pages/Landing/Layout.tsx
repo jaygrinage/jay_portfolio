153f3386b4ad3b9b55d4fe1a2e107ffc5a344e23
jaygrinage <jonahgrinage@gmail.com>
2019-12-09 15:28:18 -0600

    added dev dependency

--- src/modules/pages/Landing/Layout.tsx
+++ src/modules/pages/Landing/Layout.tsx
@@ -9,14 +9,14 @@ export default class LandingPageLayout extends React.Component {
 
   render() {
     return (
-     <main className="layout">
+      <main className="layout">
         <CirclePhoto url="https://vignette.wikia.nocookie.net/gameofthrones/images/d/d0/JonSnow8x06.PNG/revision/latest?cb=20190714094440" />
         <nav className="navigation">
-          <Kumya text="About Me" link="#" />
-          <Kumya text="Projects" link="#" />
-          <Kumya text="Contact Me" link="#" />
+          <Kumya text="About Me" hoverText="Who Me?" link="#" />
+          <Kumya text="Projects" hoverText="Check It" link="#" />
+          <Kumya text="Contact Me" hoverText="Call Me" link="#" />
         </nav>
-     </main>
+      </main>
     );
   }
 }
