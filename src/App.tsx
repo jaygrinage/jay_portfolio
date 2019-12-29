470877ee721028b424d6cb0bb259d071c667f488
jaygrinage <jonahgrinage@gmail.com>
2019-12-29 00:40:09 -0600

    rework design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { ParallaxProvider } from "react-scroll-parallax";
+
 import './App.scss';
 
 import { LandingPage } from 'modules/pages';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <ParallaxProvider>
+      <div className="App">
+        <LandingPage />
+      </div>
+    </ParallaxProvider>
   );
 }
 
--- src/modules/pages/Landing/Layout.tsx
+++ src/modules/pages/Landing/Layout.tsx
@@ -8,21 +8,30 @@ import { ReactComponent as FacebookLogo } from 'assets/svg/facebook.svg';
 import { ReactComponent as InstagramLogo } from 'assets/svg/instagram.svg';
 import { ReactComponent as TwitterLogo } from 'assets/svg/twitter.svg';
 import { ReactComponent as GoogleMail } from 'assets/svg/google.svg';
+import Tilt from "react-tilt";
 
-export default class LandingPageLayout extends React.Component {
 
-  render() {
+ 
+
+export default () => {
     return (
-      <main className="layout">
-        {/* <FlickerText lightText='Jonah' dimText='Grinage' /> */}
-        <section className="banner">
-          Jonah Grinage
-          <FlickerText text="Jonah Grinage" />
-        </section>
-        <section className="about-me">
-
-        </section>
-      </main>
+      <Tilt
+        className="Tilt"
+        options={{ max: 5 }}
+        // style={{ height: 250, width: 250 }}
+      >
+        <main className="layout">
+          {/* <FlickerText lightText='Jonah' dimText='Grinage' /> */}
+          <section className="intro">
+            <FlickerText text="Jonah Grinage" />
+            <FlickerText className="titleSub" text="Full-Stack Developer" />
+          </section>
+          <section className="about-me">
+          </section>
+          <section className="projects">
+              
+          </section>
+        </main>
+      </Tilt>
     );
-  }
 }
