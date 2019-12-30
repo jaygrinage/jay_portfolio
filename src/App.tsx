1dbf2f47e68e08db1c846a63e98cae566b6a3aff
Chuks Grinage <chuksgrinage@gmail.com>
2019-12-30 14:26:33 -0600

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
-import { ParallaxProvider } from "react-scroll-parallax";
 
 import './App.scss';
 
 import { LandingPage } from 'modules/pages';
+import Tilt from "react-tilt";
+
 
 const App: React.FC = () => {
   return (
-    <ParallaxProvider>
-      <div className="App">
+    <div className="App">
+      <Tilt
+        className="Tilt"
+        options={{
+          reverse: true,  // reverse the tilt direction
+          max: 1,     // max tilt rotation (degrees)
+          perspective: 2000,   // Transform perspective, the lower the more extreme the tilt gets.
+          scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
+          speed: 300,    // Speed of the enter/exit transition
+          transition: true,   // Set a transition on enter/exit.
+          axis: null,   // What axis should be disabled. Can be X or Y.
+          reset: false,    // If the tilt effect has to be reset on exit.
+          easing: "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+        }}
+      >
         <LandingPage />
+      </Tilt>
       </div>
-    </ParallaxProvider>
   );
 }
 
--- src/modules/pages/Landing/Layout.tsx
+++ src/modules/pages/Landing/Layout.tsx
@@ -1,5 +1,6 @@
 
 import React from "react";
+import Tilt from "react-tilt";
 
 import './Layout.scss';
 
@@ -8,18 +9,22 @@ import { ReactComponent as FacebookLogo } from 'assets/svg/facebook.svg';
 import { ReactComponent as InstagramLogo } from 'assets/svg/instagram.svg';
 import { ReactComponent as TwitterLogo } from 'assets/svg/twitter.svg';
 import { ReactComponent as GoogleMail } from 'assets/svg/google.svg';
-import Tilt from "react-tilt";
 
 
  
 
 export default () => {
-    return (
-      <Tilt
-        className="Tilt"
-        options={{ max: 5 }}
-        // style={{ height: 250, width: 250 }}
-      >
+  return (
+    <Tilt
+      className="Tilt"
+      options={{
+        reverse: true,  // reverse the tilt direction
+        max: 1,     // max tilt rotation (degrees)
+        perspective: 2000,   // Transform perspective, the lower the more extreme the tilt gets.
+        scale: 1,      // 2 = 200%, 1.5 = 150%, etc..
+        speed: 1000,    // Speed of the enter/exit transition
+      }}
+    >
         <main className="layout">
           {/* <FlickerText lightText='Jonah' dimText='Grinage' /> */}
           <section className="intro">
@@ -27,11 +32,12 @@ export default () => {
             <FlickerText className="titleSub" text="Full-Stack Developer" />
           </section>
           <section className="about-me">
+            About Me
           </section>
           <section className="projects">
-              
+              Projects
           </section>
-        </main>
-      </Tilt>
+      </main>
+    </Tilt>
     );
 }
